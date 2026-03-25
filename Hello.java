0e65aa3d77d9347783d401b8d6c8c4261984b5c5
JeongHoon1234 <thddlrna@gmail.com>
2026-03-25 18:06:36 +0900

    자바기본구조파일

---
+++ Hello.java
@@ -0,0 +1,22 @@
+package kr.com.beak;
+
+
+public class Hello {
+	
+	
+	public void print(String name) {
+		System.out.println("Hello, "+name);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello = new Hello();
+		hello.print("java");
+		
+		
+		
+	}
+
+}
